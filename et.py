b86e8464aeeb059b44abea8e1a8acb86c3b46fc7
Harshal More <Harshal.hm207@gmail.com>
2026-03-31 22:26:35 +0530

    Initial commit - CLI Expense Tracker

---
+++ et.py
@@ -0,0 +1,62 @@
+from datetime import datetime
+
+expenses = []
+total = 0
+
+while True:
+    print("\n--- Expense Tracker ---")
+    print("1. Add Expense")
+    print("2. Delete Expense")
+    print("3. Show Expenses")
+    print("4. Exit")
+
+    choice = input("Enter Choice = ")
+
+    if choice == "1":
+        name = input("Enter Name: ")
+        amount = int(input("Enter Amount: "))
+        category = input("Enter Category: ")
+        now = datetime.now().strftime("%d-%m-%Y %H:%M")
+        expenses.append((name, amount, category, now))
+        total += amount
+        print("Expense added!")
+        print("Added on:", now)
+        print("Current Total:", total)
+
+    elif choice == "2":
+        if len(expenses) == 0:
+            print("⚠ No expenses to delete")
+        else:
+            print("\nYour Expenses:")
+            for i, exp in enumerate(expenses):
+                print(i, "->", exp)
+
+            index = int(input("Enter index to delete: "))
+
+            if 0 <= index < len(expenses):
+                total -= expenses[index][1]
+                expenses.pop(index)
+                print("Expense deleted!")
+                print("Updated Total:", total)
+            else:
+                print("Invalid index")
+
+    elif choice == "3":
+        if len(expenses) == 0:
+            print("No expenses added yet.")
+        else:
+            print("\nYour Expenses:")
+            for exp in expenses:
+                print("Name:", exp[0],
+                      "| Amount:", exp[1],
+                      "| Category:", exp[2],
+                      "| Date & Time:", exp[3])
+
+            print("Total Expense:", total)
+
+    elif choice == "4":
+        print("Exiting...")
+        break
+
+    else:
+        print("Invalid choice")
